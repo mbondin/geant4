7cc38b0ffb7802f722da2d44352a6d301744198e
Michael Bondin <bondin@Michaels-MacBook-Pro-3.local>
2020-02-18 17:35:20 -0800

    removed useless shit

---
+++ PSD-build/CMakeFiles/LXe.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/LXe.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/LXe.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeActionInitialization.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeActionInitialization.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeDetectorConstruction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeDetectorConstruction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeDetectorMessenger.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeDetectorMessenger.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeEventAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeEventAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeEventMessenger.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeEventMessenger.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeHistoManager.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeHistoManager.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeMainVolume.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeMainVolume.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXePMTHit.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXePMTHit.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXePMTSD.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXePMTSD.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXePrimaryGeneratorAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXePrimaryGeneratorAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeRun.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeRun.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeRunAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeRunAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeScintHit.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeScintHit.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeScintSD.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeScintSD.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeStackingAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeStackingAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeSteppingAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeSteppingAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeSteppingMessenger.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeSteppingMessenger.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeTrackingAction.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeTrackingAction.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeTrajectory.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeTrajectory.cc.o"
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/src/LXeUserTrackInformation.cc" "/Users/bondin/Desktop/research/models/geant4_git/PSD-build/CMakeFiles/LXe.dir/src/LXeUserTrackInformation.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/bondin/Desktop/research/models/geant4_git/LXe/include"
+  "/Users/bondin/Desktop/research/geant4/geant4.10.05.p01-install/include/Geant4"
+  "/usr/local/include"
+  "/usr/local/lib/QtCore.framework"
+  "/usr/local/lib/QtCore.framework/Headers"
+  "/usr/local/include/QtGui"
+  "/usr/local/include/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PSD-build/CMakeFiles/LXe.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LXe.dir/LXe.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeActionInitialization.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeDetectorConstruction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeDetectorMessenger.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeEventAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeEventMessenger.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeHistoManager.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeMainVolume.cc.o"
+  "CMakeFiles/LXe.dir/src/LXePMTHit.cc.o"
+  "CMakeFiles/LXe.dir/src/LXePMTSD.cc.o"
+  "CMakeFiles/LXe.dir/src/LXePrimaryGeneratorAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeRun.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeRunAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeScintHit.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeScintSD.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeStackingAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeSteppingAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeSteppingMessenger.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeTrackingAction.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeTrajectory.cc.o"
+  "CMakeFiles/LXe.dir/src/LXeUserTrackInformation.cc.o"
+  "LXe"
+  "LXe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LXe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
